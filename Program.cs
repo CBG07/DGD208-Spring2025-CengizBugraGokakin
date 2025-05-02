9fc587956ac0d8c36472f73fe306896d8cccc2bf
CBG07 <bugra.bgkn@gmail.com>
2025-05-02 20:48:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace DGD208_spring2025_CengizBuğraGökakın
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
